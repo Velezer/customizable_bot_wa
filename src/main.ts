c8e64300b3ed2d9778e12577792cb0ec935787c5
Velezer <asvelezer@gmail.com>
2022-02-20 20:13:24 +0700

    create dev.todo

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import * as venom from 'venom-bot';
 import { commands } from './commands';
 import { MessageListener } from './messages'
 
-
-venom
-    .create(
+async function main() {
+    const client = await venom.create(
         'global-bot',
         (base64Qrimg, asciiQR, attempts, urlCode) => {
             console.log('\n\n\n')
@@ -12,10 +11,19 @@ venom
             console.log('\n\n\n')
         },
     )
-    .then((client) => {
-        client.onAnyMessage(async message => {
-            const mlistener = new MessageListener(message, client, commands)
-            mlistener.run()
+        .then(client => {
+            client.onAnyMessage(async message => {
+                const mlistener = new MessageListener(message, client, commands)
+                mlistener.run()
+            })
         })
-    })
-    .catch((error) => console.log(error));
+        .catch(err => {
+            console.error('main function error')
+            console.error(err)
+        })
+
+
+}
+
+
+main()
--- src/messages.ts
+++ src/messages.ts
@@ -21,14 +21,16 @@ export class MessageListener {
 
         this.commands.forEach(async command => {
             const key = message.body.split(' ')[0]
-
-            if (key === command.key && message.isGroupMsg == false) {
+            if (
+                key === command.key &&
+                message.isGroupMsg === false &&
+                message.fromMe === false
+            ) {
                 const m0 = message.body.split(' ')[0]
                 const m1 = message.body.split(' ')[1]
-                const m2 = message.body.split(`${m0} ${m1} `)[1] 
+                const m2 = message.body.split(`${m0} ${m1} `)[1]
                 command.cb(m1, m2)
 
-                console.log(this.commands)
                 await client.reply(
                     message.chatId,
                     command.replyMessage,
