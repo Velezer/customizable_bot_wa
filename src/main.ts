0e23ea8233435a293ef69601ff330e1752c5306b
Velezer <asvelezer@gmail.com>
2022-02-21 13:08:19 +0700

    release termux verion

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
 import { createConnection, QueryFailedError } from 'typeorm';
 import * as venom from 'venom-bot';
 import { Commander } from './Commander'
-import { Token } from './Token/token.entity';
-import { TokenRepo } from './Token/token.repo';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.dev.local' })
 
 
 async function main() {
-    const connection = await createConnection({
-        type: "postgres",
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT!,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DBNAME,
-        entities: [Token],
-        synchronize: true
-    });
-
-    const tokenRepo = connection.getCustomRepository(TokenRepo)
-
     const sessionName = 'global-bot'
-    const foundToken = await tokenRepo.findBySessionName(sessionName)
 
     venom.create(
         sessionName,
@@ -33,20 +17,8 @@ async function main() {
         },
         undefined,
         { browserArgs: ['--no-sandbox']},
-        foundToken?.session
     )
         .then(async client => {
-            const token = new Token()
-            token.sessionName = sessionName
-            token.session = await client.getSessionTokenBrowser()
-            try {
-                tokenRepo.save(token)
-            } catch (err) {
-                if (err instanceof QueryFailedError) {
-                    tokenRepo.update(token.id, { sessionName: token.sessionName })
-                }
-            }
-
             client.onAnyMessage(async message => {
                 const mlistener = new Commander(message, client)
                 mlistener.run()
