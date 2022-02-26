d0730167499beed3ae0ccc59f2215192cf045c76
Velezer <asvelezer@gmail.com>
2022-02-27 06:52:44 +0700

    fix

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -6,7 +6,5 @@ export class LoggerOcedBot {
     static log(botwa: BotWa, message: string) {
         botwa.sendMessage('120363038925650049@g.us', message)
     }
-    static _test(botwa: BotWa) {
-        botwa.sendButtonMessage('120363038925650049@g.us')
-    }
+
 }
