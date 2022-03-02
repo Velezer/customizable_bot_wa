9659c8b92ce364198121d57205be407e9605be43
Velezer <asvelezer@gmail.com>
2022-03-02 08:35:33 +0700

    fix pp hidden

--- src/Behavior/behaviors.ts
+++ src/Behavior/behaviors.ts
@@ -27,9 +27,10 @@ export class PromoteParticipantBehavior implements Behavior {
 
     async run(botwa: BotWa, to: string, participantJid: string): Promise<void> {
         const number = participantJid.split('@')[0]
-        const ppImg = await botwa.getProfilePictureBuffer(participantJid)
-        
-        await botwa.sendImage(to, ppImg, [participantJid])
+        const ppImg = await botwa.getProfilePictureBuffer(participantJid).then(async () => {
+            await botwa.sendImage(to, ppImg, [participantJid])
+        })
+
         await botwa.sendMentioned(to, 'promote @' + number, [participantJid])
     }
 
