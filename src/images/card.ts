fac89264b3567e291953a6423f34c1bf19f317d2
Velezer <asvelezer@gmail.com>
2022-03-03 13:37:19 +0700

    fix

--- src/images/card.ts
+++ src/images/card.ts
@@ -14,12 +14,12 @@ export class Card {
         const background = await Jimp.read(this._backgroundPath)
         const watermark = await Jimp.read(this._watermarkPath)
         watermark.circle().resize(1200, Jimp.AUTO)
-        background.composite(watermark, 2000, 100, {
+        background.composite(watermark, 2000, 50, {
             mode: Jimp.BLEND_SOURCE_OVER,
             opacityDest: 1,
             opacitySource: 1
         })
 
-        return await background.resize(1600, Jimp.AUTO).quality(50).getBufferAsync(Jimp.MIME_JPEG)
+        return await background.resize(1600, Jimp.AUTO).quality(40).getBufferAsync(Jimp.MIME_JPEG)
     }
 }
