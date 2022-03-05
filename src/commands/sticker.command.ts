d09049534230f9b92e52e5cc068134a77be08aab
Velezer <asvelezer@gmail.com>
2022-03-05 17:19:04 +0700

    /sticker

--- src/commands/sticker.command.ts
+++ src/commands/sticker.command.ts
@@ -6,6 +6,7 @@ import { BotLevel } from "../groups/interface";
 import { Command, CommandLevel } from "./interface";
 import { exec } from 'child_process'
 import fs from 'fs'
+import { removeBackgroundFromImageFile } from "remove.bg";
 
 export class StickerCommand implements Command {
     botLevel: BotLevel = BotLevel.BASIC
@@ -18,7 +19,7 @@ export class StickerCommand implements Command {
         const jid = groupChat.jid
         const input = './storage/media.jpeg'
         const output = './storage/media1.webp'
-        const img = await botwa.sock.downloadAndSaveMediaMessage(receivedMessage, input)
+        const media = await botwa.sock.downloadAndSaveMediaMessage(receivedMessage, input)
         // const jimp = await Jimp.read(img)
         // const buffer: Buffer = await jimp.getBufferAsync(Mimetype.webp)
         exec(`ffmpeg -i ${input} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${output}`, async (err) => {
@@ -28,5 +29,20 @@ export class StickerCommand implements Command {
             // fs.unlinkSync(output)
         })
 
+        const pngFile = './storage/media1.png'
+        const webpFile = './storage/media1.webp'
+
+        await removeBackgroundFromImageFile({ path: media, apiKey: 'bcAvZyjYAjKkp1cmK8ZgQvWH', size: 'auto', type: 'auto', outputFile: pngFile })
+            .then(res => {
+                // fs.unlinkSync(media)
+                let buffer = Buffer.from(res.base64img, 'base64')
+                fs.writeFileSync(pngFile, buffer)
+                exec(`ffmpeg -i ${pngFile} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${webpFile}`, async (err) => {
+                    // fs.unlinkSync(pngFile)
+                    if (err) return console.log(err)
+                    await botwa.sendSticker(jid, fs.readFileSync(webpFile))
+                    // fs.unlinkSync(webpFile)
+                })
+            })
     }
 }
