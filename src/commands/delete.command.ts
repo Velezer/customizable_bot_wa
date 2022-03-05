e2f91276764d363019c9b18ee4660926263f4791
Velezer <asvelezer@gmail.com>
2022-03-05 09:28:47 +0700

    /delete fixed

--- src/commands/delete.command.ts
+++ src/commands/delete.command.ts
@@ -15,41 +15,18 @@ export class DeleteBotTypoCommand implements Command {
     level: CommandLevel = CommandLevel.ADMIN;
 
     async run(botwa: BotWa, groupChat: GroupChat, conversation: string, quotedMessage: proto.IMessage): Promise<void> {
-        console.log('=================')
-        console.log(quotedMessage)
-        console.log('=================')
-        const quotedMessageString = quotedMessage.extendedTextMessage?.text
-        try {
-            await botwa.sock.findMessage(groupChat.jid, 20, (m) => {
-                const foundMessageString = m.message?.conversation || m.message?.extendedTextMessage?.text
-                if(m.key.fromMe === true){
-                    if (quotedMessageString === foundMessageString) {
-                        console.log('=================')
-                        console.log(quotedMessageString)
-                        console.log('=================')
-                        console.log(foundMessageString)
-                        console.log('=================')
-                        console.log(m)
-                        console.log('=================')
-                        botwa.deleteMessage(m.key)
-                        return true
-                    }
+        const quotedMessageString = quotedMessage.extendedTextMessage?.text || quotedMessage.conversation
+        await botwa.sock.findMessage(groupChat.jid, 20, (m) => {
+            const foundMessageString = m.message?.conversation || m.message?.extendedTextMessage?.text
+            if (m.key.fromMe === true) {
+                if (quotedMessageString === foundMessageString) {
+                    botwa.deleteMessage(m.key)
+                    return true
                 }
-                
-                return false
-            })
-        } catch (err) {
-            const msgKey: proto.IMessageKey = OcedBot.findMessageKey(quotedMessageString!, groupChat.jid)!
+            }
 
-            botwa.deleteMessage(msgKey!)
-                .then(() => {
-                    OcedBot.deleteReceivedMessage(msgKey!)
-                })
-                .catch(err => {
-                    botwa.sendMessage(groupChat.jid, 'delete gagal')
-                })
-        }
-
+            return false
+        })
 
     }
 
