ca556ef65fb91405c07bf1010634881321382acc
Velezer <asvelezer@gmail.com>
2022-04-23 14:03:33 +0700

    fix: forget to call

--- src/app.ts
+++ src/app.ts
@@ -112,9 +112,7 @@ export async function app(dataSource: DataSource) {
         }
 
         if (jid === LoggerOcedBot.jid) {
-            commander.unreg(conversation)
-            commander.blacklist(conversation)
-            commander.monitor(conversation)
+            commander.runLogger(conversation)
         }
 
     })
