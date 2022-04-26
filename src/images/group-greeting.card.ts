fbf4e803b0553cbfdb3eef99f0eb39482e4a0b24
Velezer <asvelezer@gmail.com>
2022-04-26 15:45:00 +0700

    chore: update kvfiledb

--- src/images/group-greeting.card.ts
+++ src/images/group-greeting.card.ts
@@ -4,20 +4,20 @@ import fs from 'fs'
 
 
 export class GroupGreetingCard extends Card {
-    constructor(type: 'welcome' | 'leave'){
-        let bg: string = type === 'welcome'? './storage/group-action-cards/OcedBot-Welcome-2.jpg' : './storage/group-action-cards/OcedBot-Leave-2.jpg'
+    constructor(type: 'welcome' | 'leave') {
+        let bg: string = type === 'welcome' ? './storage/group-action-cards/OcedBot-Welcome-2.jpg' : './storage/group-action-cards/OcedBot-Leave-2.jpg'
         super(fs.readFileSync(bg))
     }
 
-    addPP(pp: Buffer): this{
-        this.addImage(pp, 1070, Jimp.AUTO,2250, 80 )
+    addPP(pp: Buffer): this {
+        this.addImage(pp, 1070, Jimp.AUTO, 2250, 80)
         return this
     }
-    addGroupIcon(icon: Buffer): this{
-        this.addImage(icon, 360, Jimp.AUTO,285, 925 )
+    addGroupIcon(icon: Buffer): this {
+        this.addImage(icon, 360, Jimp.AUTO, 285, 925)
         return this
     }
-    addGroupTitle(text: string): this{
+    addGroupTitle(text: string): this {
         this.addText(text, 700, 1030)
         return this
     }
